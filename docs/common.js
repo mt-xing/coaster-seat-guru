c8b5db4d731040ae4da051f2f6800da21bf2b6bc
mt-xing <mt-xing@users.noreply.github.com>
2022-03-21 15:24:39 -0700

    Home Page

---
+++ docs/common.js
@@ -0,0 +1,138 @@
+const PRODUCT_NAME = 'Coaster Seat Guru';
+
+class ProductName extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'closed' }).appendChild(document.createTextNode(PRODUCT_NAME));
+	}
+}
+
+class Header extends HTMLElement {
+	constructor() {
+		super();
+
+		const shadow = this.attachShadow({ mode: 'closed' });
+		const css = shadow.appendChild(document.createElement('style'));
+		css.textContent = `
+header {
+	width: 100%;
+	background: black;
+	color: white;
+}
+
+h1 {
+	margin: 0;
+}
+		`;
+
+		const header = shadow.appendChild(document.createElement('header'));
+		const h1 = header.appendChild(document.createElement('h1'));
+		h1.textContent = PRODUCT_NAME;
+	}
+}
+
+class Search extends HTMLElement {
+	/** @type {HTMLInputElement} */
+	#input;
+
+	/** @type {HTMLElement} */
+	#resultsWrapper;
+
+	/** @type {HTMLUListElement} */
+	#results;
+
+	/** @type {number | undefined} */
+	#debounce;
+
+	constructor() {
+		super();
+		const shadow = this.attachShadow({ mode: 'closed' });
+
+		const css = shadow.appendChild(document.createElement('link'));
+		css.rel = 'stylesheet';
+		css.type = 'text/css';
+		css.href = '/css/search.css';
+
+		this.#input = shadow.appendChild(document.createElement('input'));
+		this.#input.type = 'text';
+		this.#input.placeholder = 'Search for a coaster';
+		this.#input.addEventListener('keyup', this.#changedSearch.bind(this));
+
+		this.#resultsWrapper = shadow.appendChild(document.createElement('div'));
+		this.#results = this.#resultsWrapper.appendChild(document.createElement('ul'));
+		const source = this.#resultsWrapper.appendChild(document.createElement('p'));
+		source.textContent = 'Results courtesy of ';
+		const sourceLink = source.appendChild(document.createElement('a'));
+		sourceLink.href = 'https://rcdb.com/';
+		sourceLink.rel = 'noreferrer';
+		sourceLink.textContent = 'RCDB';
+	}
+
+	#changedSearch() {
+		if (this.#debounce !== undefined) {
+			window.clearTimeout(this.#debounce);
+		}
+		this.#debounce = window.setTimeout(this.#querySearch.bind(this), 1000);
+		this.#loadList();
+	}
+
+	#loadList() {
+		if (this.#input.value === '') {
+			this.#resultsWrapper.style.display = '';
+			return;
+		}
+
+		this.#clearList();
+		const l = this.#results.appendChild(document.createElement('p'));
+		this.#resultsWrapper.style.display = 'block';
+		l.textContent = 'Loading...';
+		l.className = 'load';
+	}
+
+	#querySearch() {
+		// TODO
+		this.#debounce = undefined;
+		this.#populateList([{ name: 'Fury 325', sub: 'Carowinds', id: '1' }]);
+	}
+
+	/**
+	 * @param {{name: string, sub: string, id: string}[]} list
+	 */
+	#populateList(list) {
+		this.#clearList();
+		list.forEach((x) => this.#addChildToList(x.name, x.sub, x.id));
+		if (list.length === 0) {
+			this.#resultsWrapper.style.display = '';
+		} else {
+			this.#resultsWrapper.style.display = 'block';
+		}
+	}
+
+	#clearList() {
+		while (this.#results.firstChild) {
+			// @ts-ignore
+			this.#results.removeChild(this.#results.lastChild);
+		}
+	}
+
+	/**
+	 * @param {string} name
+	 * @param {string} sub
+	 * @param {string} id
+	 */
+	#addChildToList(name, sub, id) {
+		const li = this.#results.appendChild(document.createElement('li'));
+		const a = li.appendChild(document.createElement('a'));
+		a.href = `/results?id=${id}`;
+
+		const main = a.appendChild(document.createElement('p'));
+		main.textContent = name;
+
+		const s = a.appendChild(document.createElement('p'));
+		s.textContent = sub;
+	}
+}
+
+customElements.define('prod-name', ProductName);
+customElements.define('csg-header', Header);
+customElements.define('csg-search', Search);
