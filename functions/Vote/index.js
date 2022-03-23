9b47b112ca7f1e6d1bc7c875c367d8f641103d0d
mt-xing <mt-xing@users.noreply.github.com>
2022-03-22 18:47:23 -0700

    Remember prior contributions

--- functions/Vote/index.js
+++ functions/Vote/index.js
@@ -3,14 +3,64 @@ const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client('576763212029-fsd49s3dgcnmrpm2rcgovfkvlkhp8ube.apps.googleusercontent.com');
 
 async function verify(token) {
-	await client.verifyIdToken({
+	const ticket = await client.verifyIdToken({
 		idToken: token,
 		audience: '576763212029-fsd49s3dgcnmrpm2rcgovfkvlkhp8ube.apps.googleusercontent.com',
 	});
-	return true;
+	return ticket.getPayload().sub;
 }
 
-module.exports = async function (context, req, inputDocument) {
+/** @typedef {{id: string, submitted: Record<string, (number|null)[][]>}} UserDoc */
+
+class User {
+	/** @type {string} */
+	#sub;
+
+	/** @type {UserDoc | undefined} */
+	#doc;
+
+	/**
+	 * @param {string} sub
+	 * @param {UserDoc | undefined} doc
+	 */
+	constructor(sub, doc) {
+		this.#sub = sub;
+		this.#doc = doc;
+	}
+
+	/**
+	 * @param {string} coasterId
+	 * @returns {(number|null)[][] | null}
+	 */
+	getVotes(coasterId) {
+		if (!this.#doc) {
+			return null;
+		}
+		const c = this.#doc.submitted[coasterId];
+		if (!c) {
+			return null;
+		}
+		return c;
+	}
+
+	/**
+	 * @param {string} coasterId
+	 * @param {(number|null)[][]} votes
+	 * @returns {UserDoc}
+	 */
+	updateVotes(coasterId, votes) {
+		if (!this.#doc) {
+			this.#doc = {
+				id: this.#sub,
+				submitted: {},
+			};
+		}
+		this.#doc.submitted[coasterId] = votes;
+		return this.#doc;
+	}
+}
+
+module.exports = async function (context, req, inputDocument, userDocument) {
 	context.log('Saving changes to coaster');
 
 	if (!inputDocument) {
@@ -25,14 +75,49 @@ module.exports = async function (context, req, inputDocument) {
 		context.res = { status: 401 };
 		return;
 	}
-	try {
-		await verify(token);
-	} catch (e) {
+	const uid = await (async () => {
+		try {
+			return await verify(token);
+		} catch (e) {
+			return null;
+		}
+	})();
+	if (uid === null) {
 		context.res = { status: 401 };
 		return;
 	}
+	const user = new User(uid, userDocument);
 
-	// TODO remove user's old votes if present
+	const userPreviousVote = user.getVotes(inputDocument.id);
+	if (userPreviousVote !== null) {
+		if (userPreviousVote.length === inputDocument.rows) {
+			userPreviousVote.forEach((voteRow, r) => {
+				if (voteRow.length === inputDocument.cols) {
+					voteRow.forEach((vote, c) => {
+						switch (vote) {
+						case 1:
+							// eslint-disable-next-line no-param-reassign
+							inputDocument.data[r][c][0]--;
+							break;
+						case 2:
+							// eslint-disable-next-line no-param-reassign
+							inputDocument.data[r][c][1]--;
+							break;
+						case 3:
+							// eslint-disable-next-line no-param-reassign
+							inputDocument.data[r][c][2]--;
+							break;
+						case null:
+							break;
+						default:
+							break;
+						}
+					});
+				}
+			});
+		}
+	}
+
 	if (votes.length !== inputDocument.rows) {
 		context.res = { status: 401 };
 		return;
@@ -66,16 +151,16 @@ module.exports = async function (context, req, inputDocument) {
 		});
 	});
 
+	// TODO race conditions galore
+	context.bindings.outputUserDocument = JSON.stringify(user.updateVotes(inputDocument.id, votes));
 	context.bindings.outputDocument = JSON.stringify({
-
 		id: inputDocument.id,
 		name: inputDocument.name,
 		park: inputDocument.park,
 		rows: inputDocument.rows,
 		cols: inputDocument.cols,
 		data: inputDocument.data,
-		total: inputDocument.total + 1,
-
+		total: inputDocument.total + (userPreviousVote !== null ? 0 : 1),
 	});
 
 	context.res = {
