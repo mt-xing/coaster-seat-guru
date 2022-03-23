9959bdcb41be1690726e520998c8c17a31ca2567
mt-xing <mt-xing@users.noreply.github.com>
2022-03-22 19:31:25 -0700

    Search fn

--- functions/Search/index.js
+++ functions/Search/index.js
@@ -1,13 +1,6 @@
-module.exports = async function (context, req) {
-	context.log('JavaScript HTTP trigger function processed a request.');
-
-	const name = (req.query.name || (req.body && req.body.name));
-	const responseMessage = name
-		? `Hello, ${name}. This HTTP triggered function executed successfully.`
-		: 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
-
+module.exports = async function (context, req, inputDocument) {
 	context.res = {
 		// status: 200, /* Defaults to 200 */
-		body: responseMessage,
+		body: inputDocument.map((x) => ({ id: x.id, name: x.name, park: x.park })),
 	};
 };
