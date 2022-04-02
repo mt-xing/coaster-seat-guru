9085bff45c37e49b6b348f2053d7d79f4e9781a4
mt-xing <mt-xing@users.noreply.github.com>
2022-04-02 14:25:50 -0700

    Header and Footer Redesign

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -3,9 +3,21 @@ import styles from '../styles/Footer.module.css';
 export default function Footer(props: {isDark: boolean}) {
 	return (
 		<footer className={props.isDark ? `${styles.footer} ${styles.dark}` : styles.footer}>
-			<a href="https://www.flaticon.com/free-icons/roller-coaster" title="roller coaster icons">
-				Roller coaster icons created by Freepik - Flaticon
-			</a>
+			<div>
+				<p>
+				A website by <a href='https://michaelxing.com' target='_blank' rel='noreferrer'>Michael Xing</a>,
+				with help from <a href='https://reddit.com/r/rollercoasters' target='_blank' rel='noreferrer'>r/rollercoasters</a>.
+				</p>
+			</div>
+			<div>
+				<p>
+					{'Roller coaster icon by '}
+					<a href="https://www.flaticon.com/free-icons/roller-coaster" target='_blank' rel='noreferrer'>
+						Freepik - Flaticon
+					</a>.
+				</p>
+				<p>Proudly open source on <a href='https://github.com/mt-xing/coaster-seat-guru' target='_blank' rel='noreferrer'>GitHub</a>.</p>
+			</div>
 		</footer>
 	);
 }
