bfa8c2f7c434ce3c9a76e210ff4664e73ae57bc5
mt-xing <mt-xing@users.noreply.github.com>
2022-04-01 17:07:23 -0700

    Remove manual redirect

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  redirects: async () => [
+    {
+      source: '/contribute/newCoaster.html',
+      destination: '/contribute/newCoaster',
+      permanent: true,
+    }
+  ],
 }
 
 module.exports = nextConfig
