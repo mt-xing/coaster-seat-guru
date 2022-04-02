09cd40a22ebff5c5164cfaba48277f006bdebf80
mt-xing <mt-xing@users.noreply.github.com>
2022-04-01 21:19:30 -0700

    Attempt to show controversial seats

---
+++ client/model/heatmap.ts
@@ -0,0 +1,48 @@
+export default class HeatMap {
+	private static weightedScore(data: [number, number, number]): number {
+		return data[0] * 3 + data[1] - data[2] * 3;
+	}
+
+	private maxMagnitude: number;
+
+	private maxVotes: number;
+
+	constructor(data: [number, number, number][][]) {
+		let mm = 6;
+		let maxV = 0;
+		data.forEach((row) => {
+			row.forEach((v) => {
+				const rr = Math.abs(HeatMap.weightedScore(v));
+				if (rr > mm) {
+					mm = rr;
+				}
+				const numV = v.reduce((a, x) => a + x);
+				if (numV > maxV) { maxV = numV; }
+			});
+		});
+		this.maxMagnitude = mm;
+		this.maxVotes = maxV;
+	}
+
+	colorOfScore(data: [number, number, number]): string {
+		const voteIntensity = (Math.max(data[0], data[2]) - Math.abs(data[2] - data[0]))
+			/ (this.maxVotes / 2);
+		const yellowDeviation = Math.floor(voteIntensity * 128);
+		const yellow = [128 + yellowDeviation, 128 + yellowDeviation, 128 - yellowDeviation];
+
+		const weight = HeatMap.weightedScore(data);
+		const interpolationTarget = weight >= 0 ? [0, 256, 0] : [256, 0, 0];
+
+		const interpDistance = Math.abs(weight / this.maxMagnitude);
+
+		const colors = yellow.map(
+			(x, i) => (1 - interpDistance) * x + interpDistance * interpolationTarget[i]
+		).map((x) => (x > 255 ? 255 : x));
+
+		return `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+	}
+
+	get maximumVotes() {
+		return this.maxVotes;
+	}
+}
--- client/pages/results.tsx
+++ client/pages/results.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import {
-	useCallback, useEffect, useMemo, useState
+	useCallback, useEffect, useState
 } from 'react';
 import { SyncLoader } from 'react-spinners';
 import Link from 'next/link';
@@ -13,6 +13,7 @@ import { API_ENDPOINT, PRODUCT_NAME } from '../utils/consts';
 import styles from '../styles/Results.module.css';
 import btnStyle from '../styles/BigButton.module.css';
 import Train from '../components/train';
+import HeatMap from '../model/heatmap';
 
 type ResultsState = {
 	s: 'Loading',
@@ -21,6 +22,7 @@ type ResultsState = {
 } | ({
 	s: 'Ready',
 	selected: { row: number, col: number } | null,
+	heatmap: HeatMap,
 } & QueryResult);
 
 type QueryResult = {
@@ -32,25 +34,6 @@ type QueryResult = {
 	data: [number, number, number][][],
 	total: number,
 };
-
-function weightedScore(data: [number, number, number]): number {
-	return data[0] * 3 + data[1] - data[2] * 3;
-}
-
-function colorOfScore(data: [number, number, number], max: number): string {
-	const weight = weightedScore(data);
-	if (weight >= 0) {
-		// eslint-disable-next-line no-mixed-operators
-		const g = weight / max * 128 + 128;
-		const o = 256 - g;
-		return `rgb(${o}, ${g}, ${o})`;
-	} else {
-		// eslint-disable-next-line no-mixed-operators
-		const g = -weight / max * 128 + 128;
-		const o = 256 - g;
-		return `rgb(${g}, ${o}, ${o})`;
-	}
-}
 
 function ResultsPage() {
 	const [state, setState] = useState<ResultsState>({ s: 'Loading' });
@@ -87,27 +70,12 @@ function ResultsPage() {
 			}
 			const data = await result.json() as QueryResult;
 			window.document.title = `${data.name} Seat Map - ${PRODUCT_NAME}`;
-			setState({ s: 'Ready', ...data, selected: null });
+			setState({
+				s: 'Ready', ...data, selected: null, heatmap: new HeatMap(data.data)
+			});
 		};
 		void f();
 	}, [notFound, id, idReady]);
-
-	const [minmax, maxVotes] = useMemo(() => {
-		if (state.s !== 'Ready') { return [NaN, NaN]; }
-		let mm = 6;
-		let maxV = 0;
-		state.data.forEach((row) => {
-			row.forEach((v) => {
-				const rr = Math.abs(weightedScore(v));
-				if (rr > mm) {
-					mm = rr;
-				}
-				const numV = v.reduce((a, x) => a + x);
-				if (numV > maxV) { maxV = numV; }
-			});
-		});
-		return [mm, maxV];
-	}, [state]);
 
 	const getSelectionDetails = useCallback((s: ResultsState) => {
 		if (s.s !== 'Ready') { return null; }
@@ -127,7 +95,7 @@ function ResultsPage() {
 						return <tr key={voteType[0]}>
 							<th>{voteType[0]}</th>
 							<td><span style={{
-								width: `${max === 0 ? 0 : (((v / maxVotes) * 100) / 2)}%`,
+								width: `${max === 0 ? 0 : (((v / s.heatmap.maximumVotes) * 100) / 2)}%`,
 								backgroundColor: voteType[1],
 							}}></span>{`${max === 0 ? 0 : (Math.round((v / voteTotal) * 100))}% (${v})`}</td>
 						</tr>;
@@ -138,7 +106,7 @@ function ResultsPage() {
 				</tbody>
 			</table>
 		</>;
-	}, [maxVotes]);
+	}, []);
 
 	return useCallback(() => {
 		switch (state.s) {
@@ -160,7 +128,7 @@ function ResultsPage() {
 					rows={state.rows}
 					cols={state.cols}
 					render={(r, c) => <button
-						style={{ backgroundColor: colorOfScore(state.data[r][c], minmax) }}
+						style={{ backgroundColor: state.heatmap.colorOfScore(state.data[r][c]) }}
 						onClick={() => setSelected(r, c)}
 					></button>}
 				/>
@@ -178,7 +146,7 @@ function ResultsPage() {
 		default:
 			return assertUnreachable(state);
 		}
-	}, [id, state, minmax, setSelected, getSelectionDetails])();
+	}, [id, state, setSelected, getSelectionDetails])();
 }
 
 const Results: NextPage = () => (
