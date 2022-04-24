3e5b0788ceb5f864f312352602f900ac4fdff8e5
mt-xing <mt-xing@users.noreply.github.com>
2022-04-24 16:31:33 -0700

    Tab fix

--- functions/getCoaster/index.ts
+++ functions/getCoaster/index.ts
@@ -1,31 +1,31 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
 const httpTrigger: AzureFunction = function (
-  context: Context, req: HttpRequest, inputDocument,
+	context: Context, req: HttpRequest, inputDocument,
 ): void {
-  context.log('Fetching coaster');
+	context.log('Fetching coaster');
 
-  if (!inputDocument) {
-    context.res = {
-      status: 404,
-    };
-    return;
-  }
+	if (!inputDocument) {
+		context.res = {
+			status: 404,
+		};
+		return;
+	}
 
-  const resp = {
-    id: inputDocument.id,
-    name: inputDocument.name,
-    park: inputDocument.park,
-    rows: inputDocument.rows,
-    cols: inputDocument.cols,
-    data: inputDocument.data,
-    total: inputDocument.total,
-  };
+	const resp = {
+		id: inputDocument.id,
+		name: inputDocument.name,
+		park: inputDocument.park,
+		rows: inputDocument.rows,
+		cols: inputDocument.cols,
+		data: inputDocument.data,
+		total: inputDocument.total,
+	};
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: JSON.stringify(resp),
-  };
+	context.res = {
+		// status: 200, /* Defaults to 200 */
+		body: JSON.stringify(resp),
+	};
 };
 
 export default httpTrigger;
--- functions/search/index.ts
+++ functions/search/index.ts
@@ -1,12 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
 const httpTrigger: AzureFunction = function (
-  context: Context, req: HttpRequest, inputDocument,
+	context: Context, req: HttpRequest, inputDocument,
 ): void {
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: inputDocument.map((x) => ({ id: x.id, name: x.name, park: x.park })),
-  };
+	context.res = {
+		// status: 200, /* Defaults to 200 */
+		body: inputDocument.map((x) => ({ id: x.id, name: x.name, park: x.park })),
+	};
 };
 
 export default httpTrigger;
