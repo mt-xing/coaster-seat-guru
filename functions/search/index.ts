b2a40a347b70d258dd35fb15e1695d86b62d3b6d
mt-xing <mt-xing@users.noreply.github.com>
2022-04-24 16:18:21 -0700

    Line ending fixes

--- functions/search/index.ts
+++ functions/search/index.ts
@@ -1,10 +1,10 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, inputDocument): Promise<void> {
-    context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: inputDocument.map((x) => ({ id: x.id, name: x.name, park: x.park })),
-    };
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: inputDocument.map((x) => ({ id: x.id, name: x.name, park: x.park })),
+  };
 };
 
 export default httpTrigger;
--- functions/vote/index.ts
+++ functions/vote/index.ts
@@ -1,4 +1,4 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { OAuth2Client } from 'google-auth-library';
 
 const client = new OAuth2Client('707815788715-v292qtutlmval10742tekpbnv2a6to6l.apps.googleusercontent.com');
@@ -14,158 +14,157 @@ async function verify(token: string) {
 type UserDoc = {
     id: string, submitted: Record<string, (number|null)[][]>
   };
-  
-  class User {
-    /** @type {string} */
-    #sub;
-  
-    /** @type {UserDoc | undefined} */
-    #doc;
-  
-    constructor(sub: string, doc: UserDoc | undefined) {
-      this.#sub = sub;
-      this.#doc = doc;
-    }
-  
-    /**
+
+class User {
+  /** @type {string} */
+  #sub;
+
+  /** @type {UserDoc | undefined} */
+  #doc;
+
+  constructor(sub: string, doc: UserDoc | undefined) {
+    this.#sub = sub;
+    this.#doc = doc;
+  }
+
+  /**
        * @param {string} coasterId
        * @returns {(number|null)[][] | null}
        */
-    getVotes(coasterId) {
-      if (!this.#doc) {
-        return null;
-      }
-      const c = this.#doc.submitted[coasterId];
-      if (!c) {
-        return null;
-      }
-      return c;
+  getVotes(coasterId) {
+    if (!this.#doc) {
+      return null;
     }
-  
-    /**
+    const c = this.#doc.submitted[coasterId];
+    if (!c) {
+      return null;
+    }
+    return c;
+  }
+
+  /**
        * @param {string} coasterId
        * @param {(number|null)[][]} votes
        * @returns {UserDoc}
        */
-    updateVotes(coasterId, votes) {
-      if (!this.#doc) {
-        this.#doc = {
-          id: this.#sub,
-          submitted: {},
-        };
-      }
-      this.#doc.submitted[coasterId] = votes;
-      return this.#doc;
-    }
-  }
-  
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, inputDocument, userDocument): Promise<void> {
-
-    context.log('Saving changes to coaster');
-
-    if (!inputDocument) {
-      context.res = {
-        status: 404,
+  updateVotes(coasterId, votes) {
+    if (!this.#doc) {
+      this.#doc = {
+        id: this.#sub,
+        submitted: {},
       };
-      return;
     }
-  
-    const { token, votes } = req.body;
-    if (!token || !votes) {
-      context.res = { status: 401 };
-      return;
+    this.#doc.submitted[coasterId] = votes;
+    return this.#doc;
+  }
+}
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, inputDocument, userDocument): Promise<void> {
+  context.log('Saving changes to coaster');
+
+  if (!inputDocument) {
+    context.res = {
+      status: 404,
+    };
+    return;
+  }
+
+  const { token, votes } = req.body;
+  if (!token || !votes) {
+    context.res = { status: 401 };
+    return;
+  }
+  const uid = await (async () => {
+    try {
+      return await verify(token);
+    } catch (e) {
+      return null;
     }
-    const uid = await (async () => {
-      try {
-        return await verify(token);
-      } catch (e) {
-        return null;
-      }
-    })();
-    if (uid === null) {
-      context.res = { status: 401 };
-      return;
-    }
-    const user = new User(uid, userDocument);
-  
-    const userPreviousVote = user.getVotes(inputDocument.id);
-    if (userPreviousVote !== null) {
-      if (userPreviousVote.length === inputDocument.rows) {
-        userPreviousVote.forEach((voteRow, r) => {
-          if (voteRow.length === inputDocument.cols) {
-            voteRow.forEach((vote, c) => {
-              switch (vote) {
-                case 1:
-                  // eslint-disable-next-line no-param-reassign
-                  inputDocument.data[r][c][0]--;
-                  break;
-                case 2:
-                  // eslint-disable-next-line no-param-reassign
-                  inputDocument.data[r][c][1]--;
-                  break;
-                case 3:
-                  // eslint-disable-next-line no-param-reassign
-                  inputDocument.data[r][c][2]--;
-                  break;
-                case null:
-                  break;
-                default:
-                  break;
-              }
-            });
-          }
-        });
-      }
-    }
-  
-    if (votes.length !== inputDocument.rows) {
-      context.res = { status: 401 };
-      return;
-    }
-    votes.forEach((voteRow, r) => {
-      if (voteRow.length !== inputDocument.cols) {
-        context.res = { status: 401 };
-        return;
-      }
-      voteRow.forEach((vote, c) => {
-        // vote === 1, 2, or 3, or undefined
-        switch (vote) {
-          case 1:
-            // eslint-disable-next-line no-param-reassign
-            inputDocument.data[r][c][0]++;
-            break;
-          case 2:
-            // eslint-disable-next-line no-param-reassign
-            inputDocument.data[r][c][1]++;
-            break;
-          case 3:
-            // eslint-disable-next-line no-param-reassign
-            inputDocument.data[r][c][2]++;
-            break;
-          case null:
-            break;
-          default:
-            context.res = { status: 400 };
+  })();
+  if (uid === null) {
+    context.res = { status: 401 };
+    return;
+  }
+  const user = new User(uid, userDocument);
+
+  const userPreviousVote = user.getVotes(inputDocument.id);
+  if (userPreviousVote !== null) {
+    if (userPreviousVote.length === inputDocument.rows) {
+      userPreviousVote.forEach((voteRow, r) => {
+        if (voteRow.length === inputDocument.cols) {
+          voteRow.forEach((vote, c) => {
+            switch (vote) {
+              case 1:
+                // eslint-disable-next-line no-param-reassign
+                inputDocument.data[r][c][0]--;
+                break;
+              case 2:
+                // eslint-disable-next-line no-param-reassign
+                inputDocument.data[r][c][1]--;
+                break;
+              case 3:
+                // eslint-disable-next-line no-param-reassign
+                inputDocument.data[r][c][2]--;
+                break;
+              case null:
+                break;
+              default:
+                break;
+            }
+          });
         }
       });
+    }
+  }
+
+  if (votes.length !== inputDocument.rows) {
+    context.res = { status: 401 };
+    return;
+  }
+  votes.forEach((voteRow, r) => {
+    if (voteRow.length !== inputDocument.cols) {
+      context.res = { status: 401 };
+      return;
+    }
+    voteRow.forEach((vote, c) => {
+      // vote === 1, 2, or 3, or undefined
+      switch (vote) {
+        case 1:
+          // eslint-disable-next-line no-param-reassign
+          inputDocument.data[r][c][0]++;
+          break;
+        case 2:
+          // eslint-disable-next-line no-param-reassign
+          inputDocument.data[r][c][1]++;
+          break;
+        case 3:
+          // eslint-disable-next-line no-param-reassign
+          inputDocument.data[r][c][2]++;
+          break;
+        case null:
+          break;
+        default:
+          context.res = { status: 400 };
+      }
     });
-  
-    // TODO race conditions galore
-    context.bindings.outputUserDocument = JSON.stringify(user.updateVotes(inputDocument.id, votes));
-    context.bindings.outputDocument = JSON.stringify({
-      id: inputDocument.id,
-      name: inputDocument.name,
-      searchName: inputDocument.searchName,
-      park: inputDocument.park,
-      rows: inputDocument.rows,
-      cols: inputDocument.cols,
-      data: inputDocument.data,
-      total: inputDocument.total + (userPreviousVote !== null ? 0 : 1),
-    });
-  
-    context.res = {
-      status: 200,
-    };
+  });
+
+  // TODO race conditions galore
+  context.bindings.outputUserDocument = JSON.stringify(user.updateVotes(inputDocument.id, votes));
+  context.bindings.outputDocument = JSON.stringify({
+    id: inputDocument.id,
+    name: inputDocument.name,
+    searchName: inputDocument.searchName,
+    park: inputDocument.park,
+    rows: inputDocument.rows,
+    cols: inputDocument.cols,
+    data: inputDocument.data,
+    total: inputDocument.total + (userPreviousVote !== null ? 0 : 1),
+  });
+
+  context.res = {
+    status: 200,
+  };
 };
 
 export default httpTrigger;
