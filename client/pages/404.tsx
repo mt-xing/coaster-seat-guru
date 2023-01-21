648a3c6e76220ffd3526132383a5a499be5486a1
mt-xing <mt-xing@users.noreply.github.com>
2023-01-21 11:50:09 -0800

    Custom 404 snark

---
+++ client/pages/404.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Footer from '../components/footer';
+import Header from '../components/header';
+import { PRODUCT_NAME } from '../utils/consts';
+import styles from '../styles/Results.module.css';
+
+export default function Custom404() {
+	return <>
+		<Head>
+			<title>Seat Map - {PRODUCT_NAME}</title>
+			<meta name="description" content="Coaster seat map" />
+		</Head>
+		<Header />
+		<main className={styles.main}>
+			<h1 style={{ fontSize: '1000%', margin: 0 }}>404</h1>
+			<h2 style={{ fontSize: '300%' }}>Page Not Found</h2>
+			<p>Like Six Flags' corporate strategy, this page doesn't seem to exist.</p>
+			<p>Why not try searching for a coaster?</p>
+		</main>
+		<Footer isDark={false} />
+	</>;
+}
